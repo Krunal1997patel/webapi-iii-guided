a5cba91fc1260c127377d0274ba78cc78b564328
Krunal Patel <Pkrunal616@gmail.com>
2019-12-04 13:14:11 -0500

    gatekeeper

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require('express'); // importing a CommonJS module
+const helmet = require('helmet');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+
+function gateKeeper(req, res, next) {
+  // data can come in the body, url parameters, query string, headers
+  // new way of reading data sent by the client
+  const password = req.headers.password || '';
+  if(!password) {
+    return res.status(400).json({ message: 'please provide a password!!' });
+  }
+  if (password.toLowerCase() === 'mellon') {
+    next();
+  } else {
+    res.status(400).json({ you: 'cannot pass!!' });
+  }
+}
+
+
+server.use(helmet());
 server.use(express.json());
 
 server.use('/api/hubs', hubsRouter);
